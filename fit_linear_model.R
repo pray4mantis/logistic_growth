ff31cdd80b80efc3779a547399aba8de4fcf234c
pray4mantis <151776528+pray4mantis@users.noreply.github.com>
2023-11-30 13:13:05 +0000

    Update fit_linear_model.R

--- fit_linear_model.R
+++ fit_linear_model.R
@@ -1,19 +1,25 @@
 #Script to estimate the model parameters using a linear approximation
 
-#library(dplyr)
+install.packages('dplyr')
+library(dplyr)
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment3.csv")
 
 #Case 1. K >> N0, t is small
 
-data_subset1 <- growth_data %>% filter(t<???) %>% mutate(N_log = log(N))
+data_subset1 <- growth_data %>% filter(t<2500) %>% mutate(N_log = log(N))
 
+#Creating a linear model for the very start of the graph when the growth rate = r
+#This model shows the relationship between log(N) and T
+#So the intercept is log(N0), and the t is the gradient, or r
 model1 <- lm(N_log ~ t, data_subset1)
 summary(model1)
 
 #Case 2. N(t) = K
 
-data_subset2 <- growth_data %>% filter(t>???)
+data_subset2 <- growth_data %>% filter(t>4000)
 
+#This is a linear model for the end of the graph when N = K
+#The intercept shows you an estimate of K
 model2 <- lm(N ~ 1, data_subset2)
 summary(model2)
